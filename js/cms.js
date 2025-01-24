1597ddd3675bdac6c36bbb8c5110d7fded6bfd8e
theerondu <erone007@gmail.com>
2025-01-24 21:35:51 +0100

    initial commit

---
+++ js/cms.js
@@ -0,0 +1,23 @@
+// script.js
+
+// Function to load the header content
+function loadHeader() {
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        });
+}
+
+// Function to load the footer content
+function loadFooter() {
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        });
+}
+
+// Call the functions to load header and footer
+loadHeader();
+loadFooter();
